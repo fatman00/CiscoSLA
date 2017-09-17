b793bb4bf4b7f372b0a23f1a719034ca7348cd8c
fatman00 <root@fubar.snarfu.gl>
2017-09-17 17:25:16 -0200

    Added graphs for udpjitter

--- pnp-templates/check_mk-cisco_sla_udpjitter.php
+++ pnp-templates/check_mk-cisco_sla_udpjitter.php
@@ -25,7 +25,7 @@
 
 $desc = str_replace("_", " ", $servicedesc);
 
-$opt[1] = "-X0 --vertical-label \"Response Time (ms)\"  --title \"$hostname / $desc\" ";
+$opt[1] = "-X0 --vertical-label \"Response Time (ms)\"  --title \"$hostname / $desc / RTT\" ";
 
 $def[1] = ""
  . "DEF:var1=$RRDFILE[1]:$DS[1]:MAX "
@@ -34,5 +34,29 @@ $def[1] = ""
  . "LINE1:ms#000000:\"\" "
  . "GPRINT:ms:LAST:\"%3.3lg ms LAST \" "
  . "GPRINT:ms:MAX:\"%3.3lg ms MAX \" "
- . "GPRINT:ms:AVERAGE:\"%3.3lg ms AVERAGE \" "
+ . "GPRINT:ms:AVERAGE:\"%3.3lg ms AVERAGE \" ";
+
+
+if (isset($RRDFILE[2]) && isset($RRDFILE[3])) {
+//if ( 0 ) {
+
+
+$opt[2] = "-X0 --vertical-label \"Response Time (ms)\"  --title \"$hostname / $desc / OneWay\" ";
+
+$def[2] = "DEF:var2=$RRDFILE[2]:$DS[1]:MAX ";
+$def[2] .= "CDEF:sd=var2,1,* ";
+$def[2] .= "LINE1:sd#d020a0:\"Source->Destination Latency\" ";
+$def[2] .= "GPRINT:sd:LAST:\"%3.3lg ms LAST \" ";
+$def[2] .= "GPRINT:sd:MAX:\"%3.3lg ms MAX \" ";
+$def[2] .= "GPRINT:sd:AVERAGE:\"%3.3lg ms AVERAGE \" ";
+
+
+$def[2] .= "DEF:var3=$RRDFILE[3]:$DS[1]:MAX ";
+$def[2] .= "CDEF:ds=var3,1,* ";
+$def[2] .= "LINE1:ds#d08400:\"Destination->Source Latency\" ";
+$def[2] .= "GPRINT:ds:LAST:\"%3.3lg ms LAST \" ";
+$def[2] .= "GPRINT:ds:MAX:\"%3.3lg ms MAX \" ";
+$def[2] .= "GPRINT:ds:AVERAGE:\"%3.3lg ms AVERAGE \" ";
+
+}
 ?>
